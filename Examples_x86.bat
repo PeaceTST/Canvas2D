ef456dfbbc5cc8cda8580b03142034937c4c36f3
Testaware <testaware@noreply.codeberg.org>
2026-05-07 11:22:58 +0200

    Dateien nach „/“ hochladen

---
+++ Examples_x86.bat
@@ -0,0 +1,5 @@
+@rem 0=BAT / 1=First Parameter / d=DRIVE / p=PATH / n=FILE
+set	vexe=Tools\BootMenu\C2D_BootMenu_x86.exe
+set	vpath=Executables\
+set	vtitle=CANVAS_2D_EXAMPLES
+start "" /B ""%vexe% %vpath% %vtitle%""
---
+++ MegaDemo_x64.bat
@@ -0,0 +1,5 @@
+@rem 0=BAT / 1=First Parameter / d=DRIVE / p=PATH / n=FILE
+set	vexe=Tools\BootMenu\C2D_BootMenu_x64.exe
+set	vpath=Demos\
+set	vtitle=CANVAS_2D_MEGADEMO
+start "" /B ""%vexe% %vpath% %vtitle%""
